975abb4d30e4049e55a33ab40e4958dc9f7c495b
love306 <j127289j@gmail.com>
2019-08-24 17:30:46 +0800

    update'

--- import.py
+++ import.py
@@ -1,15 +1,18 @@
 import random
 r = random.randint(1, 100)
-
+count = 0
 while True:
 	
 	g = input('猜數字')
 	g = int(g)
+	count +=  1
 	if g == r:
 		print("猜對啦!賽耶!")
+		print('猜', count,'次')
 		break
 	else:
 		if g > r:
 			print("比答案大")
 		else:
 			print("比答案小")
+		print('猜', count,'次')
